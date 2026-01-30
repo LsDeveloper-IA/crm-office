22c71bd84a5a4b4bdcfb4965441064e93f2694cb
Tiago Italo <italomoura188@gmail.com>
2026-01-30 12:37:03 -0300

    feat: api de cadastro de novos usuários

--- app/api/newUser/route.ts
+++ app/api/newUser/route.ts
@@ -4,14 +4,33 @@ import bcrypt from "bcryptjs";
 import { isValidEmail } from "@/lib/email";
 import { isValidPassword } from "@/lib/password";
 
+type NewUserRequest = {
+    name: string;
+    username: string;
+    password: string;
+    role: string;
+    active?: boolean;
+};
 
 // POST /api/newUser
 export async function POST(req: NextRequest) {
     try {
-        const body = await req.json();
-        const name = body.name.toString().trim();
+        const body: NewUserRequest = await req.json();
+
+        // Verifica se todas as informações foram enviadas com sucesso
+        if (!body.name || !body.username || !body.password || !body.role) {
+            return NextResponse.json(
+                { error: "Campos obrigatórios não informados" },
+                { status: 400 }
+            );
+        }
+
+        const active = Boolean(body.active);
+        const name = body.name.toString();
         const email = body.username.toString().trim();
         const password = body.password.toString();
+        const role = body.role.toString();
+        const saltRounds = 10;
 
         if (!isValidEmail(email)) {
             return NextResponse.json(
@@ -30,11 +49,16 @@ export async function POST(req: NextRequest) {
         await prisma.$transaction(async (tx) => {
             await tx.user.upsert({
                 where: { username: email },
-                update: {},
+                update: {
+                    active: active,
+                    name: name,
+                    role: role,
+                },
                 create: {
                     username: email,
                     name: name,
-                    password: password,
+                    password: await bcrypt.hash(password, saltRounds),
+                    role: role,
                 }
             });
         });
@@ -44,5 +68,9 @@ export async function POST(req: NextRequest) {
 
     catch (err: any) {
         console.error("POST /api/newUser ERROR: ", err);
+        return NextResponse.json(
+            { error: "Erro interno do servidor" },
+            { status: 500 }
+        )
     }
 }   
